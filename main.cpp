820afc773ae3b3eb346bedbf07cbda905b43085d
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2024-05-11 18:59:55 -0400

    modify code to not require Eigen

--- main.cpp
+++ main.cpp
@@ -4,9 +4,7 @@
 #endif
 #include "colormap.h"
 #include <fstream>
-#include <Eigen/Dense>
 #include <opencv2/opencv.hpp>
-#include <opencv2/core/eigen.hpp>
 #include <string>
 #include <sstream>
 #include <map>
@@ -19,50 +17,30 @@
 #define IM_HEIGHT 1000
 #define IM_WIDTH 1000
 #define N_ITER 380
-#define FRACTAL_JULIA 2
 #define FRACTAL_MANDELBROT 1
+#define FRACTAL_JULIA 2
 #define FRACTAL_BURNINGSHIP 3
 
 // using Eigen::ArrayXXd;
 // using namespace Eigen;
 // using namespace std;
 
-// https://gitlab.math.ethz.ch/NumCSE/NumCSE/blob/3c723d06ffacab3dc45726ad0a95e33987dc35aa/Utils/meshgrid.hpp
-//! Generates a mesh, just like Matlab's meshgrid
-//  Template specialization for column vectors (Eigen::VectorXd)
-//  in : x, y column vectors 
-//       X, Y matrices, used to save the mesh
-template <typename Scalar>
-void meshgrid(const Eigen::Matrix<Scalar, -1, 1>& x, 
-              const Eigen::Matrix<Scalar, -1, 1>& y,
-              Eigen::Matrix<Scalar, -1, -1>& X,
-              Eigen::Matrix<Scalar, -1, -1>& Y) {
-  const long nx = x.size(), ny = y.size();
-  X.resize(ny, nx);
-  Y.resize(ny, nx);
-  for (long i = 0; i < ny; ++i) {
-    X.row(i) = x.transpose();
-  }
-  for (long j = 0; j < nx; ++j) {
-    Y.col(j) = y;
-  }
+std::vector<double> linspace(double min, double max, int n)
+{
+	std::vector<double> result;
+	int iterator = 0;
+
+	for (int i = 0; i <= n-2; i++)	
+	{
+		double temp = min + i*(max-min)/(floor((double)n) - 1);
+		result.insert(result.begin() + iterator, temp);
+		iterator += 1;
+	}
+	result.insert(result.begin() + iterator, max);
+	return result;
 }
 
 const double pi = std::acos(-1.0);
-
-//! Generates a mesh, just like Matlab's meshgrid
-//  Template specialization for row vectors (Eigen::RowVectorXd)
-//  in : x, y row vectors 
-//       X, Y matrices, used to save the mesh
-template <typename Scalar>
-void meshgrid(const Eigen::Matrix<Scalar, 1, -1>& x, 
-              const Eigen::Matrix<Scalar, 1, -1>& y,
-              Eigen::Matrix<Scalar, -1, -1>& X,
-              Eigen::Matrix<Scalar, -1, -1>& Y) {
-  Eigen::Matrix<Scalar, -1, 1> xt = x.transpose(),
-                               yt = y.transpose();
-  meshgrid(xt, yt, X, Y);
-}
 
 std::string toLower(std::string s){
   std::transform(s.begin(), s.end(), s.begin(), ::tolower);
@@ -72,6 +50,7 @@ std::string toLower(std::string s){
 class Fractal {
   public:
     int formula;
+    std::string formulaStr, cmapStr;
     float nf;
     double xmin, xmax, ymin, ymax, res;
     int width, height;
@@ -79,12 +58,12 @@ class Fractal {
     bool onSecondClick;
     int cmap;
     double c_real, c_imag;
-    Eigen::VectorXd xs, ys;
-    Eigen::ArrayXXd escapetime;
+    std::vector<double> xs, ys;
+    cv::Mat escapetime;
     cv::Mat imageR;
     Fractal(int f, double xm, double xx, double ym, double yx, double r);
     Fractal(const std::string& s);
-    Eigen::ArrayXXd compute(const int& n_iter);
+    void compute(const int& n_iter);
     void colorize(const double& val, const int& i, const int& j);
     void otherInit();
 };
@@ -92,9 +71,12 @@ class Fractal {
 void Fractal::otherInit(){
   width = (int)((xmax-xmin)*res);
   height = (int)((ymax-ymin)*res);
+  escapetime = cv::Mat(cv::Size(width, height), CV_64F);
+
   onSecondClick = false;
-  xs = Eigen::VectorXd::LinSpaced(width, xmin, xmax); //x coordinates in order (for meshgrid)
-  ys = Eigen::VectorXd::LinSpaced(height, ymin, ymax).reverse();  //y coordinates in order (for meshgrid)
+  xs = linspace(xmin, xmax, width);
+  ys = linspace(ymin, ymax, height);
+  std::reverse(ys.begin(), ys.end());
   imageR = cv::Mat(height, width, CV_8UC3, cv::Scalar(0,0,0));
 }
 
@@ -150,12 +132,12 @@ Fractal::Fractal(const std::string& conffile){
   if(config.find("formula")==config.end()){
     std::cerr << "formula missing from config.\n";
   } else {
-    const std::string formulaStr = config.at("formula");
-    if(toLower(formulaStr)=="mandelbrot"){
+    formulaStr = toLower(config.at("formula"));
+    if(formulaStr=="mandelbrot"){
       formula=FRACTAL_MANDELBROT;
-    } else if(toLower(formulaStr)=="julia"){
+    } else if(formulaStr=="julia"){
       formula=FRACTAL_JULIA;
-    } else if(toLower(formulaStr)=="burningship"){
+    } else if(formulaStr=="burningship"){
       formula=FRACTAL_BURNINGSHIP;
     } else {
       std::cerr << "The formula " << formulaStr << " is not a valid option.\n";
@@ -164,7 +146,7 @@ Fractal::Fractal(const std::string& conffile){
   if(config.find("cmap")==config.end()){
     cmap=GRAYSCALE;
   } else {  // select the colormap
-    const std::string cmapStr = toLower(config.at("cmap"));
+    cmapStr = toLower(config.at("cmap"));
     if(cmapStr=="magma"){
       cmap=MAGMA;
     } else if(cmapStr=="rainbow"){
@@ -194,13 +176,14 @@ Fractal::Fractal(const std::string& conffile){
   otherInit();
 }
 
-Eigen::ArrayXXd Fractal::compute(const int& n_iter){
-  escapetime = Eigen::ArrayXXd(height, width);
+ void Fractal::compute(const int& n_iter){
   float a=0, b=0;
   float atmp;
   long colorval;
+  double* ptr;
   // printf("width=%d  height=%d\n", width, height);
   for(int j=0; j<height; j++){
+    ptr = escapetime.ptr<double>(j);
     for(int i=0; i<width; i++){
       int n=1;
       a=0;
@@ -208,23 +191,23 @@ Eigen::ArrayXXd Fractal::compute(const int& n_iter){
       atmp=0;
       if(formula==FRACTAL_MANDELBROT){
         while(a*a+b*b<4 && n<=n_iter){
-          atmp = a*a - b*b + xs(i);
-          b = (a+a)*b + ys(j);
+          atmp = a*a - b*b + xs.at(i);
+          b = (a+a)*b + ys.at(j);
           a = atmp;
           n++;
         }
       } else if(formula==FRACTAL_BURNINGSHIP){
-        a=xs(i);
-        b=ys(j);
+        a=xs.at(i);
+        b=ys.at(j);
         while(a*a+b*b<4 && n<=n_iter){
-          atmp = a*a - b*b - xs(i);
-          b = abs(2.0*a*b) - ys(j);
+          atmp = a*a - b*b - xs.at(i);
+          b = abs(2.0*a*b) - ys.at(j);
           a = atmp;
           n++;
         }
       } else if(formula==FRACTAL_JULIA){
-        a=xs(i);
-        b=ys(j);
+        a=xs.at(i);
+        b=ys.at(j);
         while(a*a+b*b<4 && n<=n_iter){
           atmp = a*a - b*b + c_real;
           b = (a+a)*b + c_imag;
@@ -235,20 +218,19 @@ Eigen::ArrayXXd Fractal::compute(const int& n_iter){
       if(formula==FRACTAL_MANDELBROT){
         nf = (float)n;
         if (n>=n_iter){
-          escapetime(j,i) = 1.0;
+          ptr[i] = 1.0;
         } else {
           // printf("%f\n", log(log(a*a+b*b)/2.0f)/log(2));
           nf = nf - log(log(a*a+b*b)/2.0f)/log(2);
-          escapetime(j,i) = nf/((double)n_iter);
+          ptr[i] = nf/((double)n_iter);
         }
       } else {
-        escapetime(j,i) = n>=n_iter ? 1.0 : n/((double)n_iter);
+        ptr[i] = n>=n_iter ? 1.0 : n/((double)n_iter);
       }
-      colorize(escapetime(j,i), j, i);
+      colorize(ptr[i], j, i);
     }
   }
   // return escapetime/((double)n_iter);
-  return escapetime;
 }
 
 void Fractal::colorize(const double& val, const int& i, const int& j){
@@ -270,18 +252,18 @@ void editWindowCallback(int event, int x, int y, int flags, void* userdata){
       frac->lowerRight[1] = y;
 
       if(frac->lowerRight[0] > frac->topLeft[0]){
-        frac->xmin = frac->xs(frac->topLeft[0]);
-        frac->xmax = frac->xs(frac->lowerRight[0]);
+        frac->xmin = frac->xs.at(frac->topLeft[0]);
+        frac->xmax = frac->xs.at(frac->lowerRight[0]);
       } else {
-        frac->xmin = frac->xs(frac->lowerRight[0]);
-        frac->xmax = frac->xs(frac->topLeft[0]);
+        frac->xmin = frac->xs.at(frac->lowerRight[0]);
+        frac->xmax = frac->xs.at(frac->topLeft[0]);
       }
       if(frac->lowerRight[1] > frac->topLeft[1]){
-        frac->ymax = frac->ys(frac->topLeft[1]);
-        frac->ymin = frac->ys(frac->lowerRight[1]);
+        frac->ymax = frac->ys.at(frac->topLeft[1]);
+        frac->ymin = frac->ys.at(frac->lowerRight[1]);
       } else {
-        frac->ymax = frac->ys(frac->lowerRight[1]);
-        frac->ymin = frac->ys(frac->topLeft[1]);
+        frac->ymax = frac->ys.at(frac->lowerRight[1]);
+        frac->ymin = frac->ys.at(frac->topLeft[1]);
       }
       frac->res = frac->width/(frac->xmax - frac->xmin);
 
@@ -293,15 +275,13 @@ void editWindowCallback(int event, int x, int y, int flags, void* userdata){
       frac->topLeft[1] = y;
       frac->onSecondClick = true;
     }
-    std::cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << std::endl;
+    // std::cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << std::endl;
   }
 }
 
 int main(int argc, char **argv)
 {
   Fractal frac = Fractal("fractalconf.cfg");
-
-  Eigen::setNbThreads(16);
 
   frac.compute(N_ITER);
   
@@ -310,9 +290,17 @@ int main(int argc, char **argv)
   cv::imshow("Fractal", frac.imageR);
   cv::waitKey(0);
   if(argc >= 2){  // save if a file path was provided
-    const char *fmt_str = "%s/%.6f_%.6f_res%.1f.png";
     char* result;
-    asprintf(&result, fmt_str, argv[1],frac.xmin,frac.ymin, frac.res);
+    const char *fmt_str;
+    if(frac.formula!=FRACTAL_JULIA){
+    fmt_str = "%s/%s_%s_%.5g_%.5g_res%.7g.png";
+    asprintf(&result, fmt_str, argv[1], frac.formulaStr.c_str(), 
+        frac.cmapStr.c_str(), frac.xmin, frac.ymin, frac.res);
+    } else {
+      fmt_str = "%s/%s_%s_%.6g+%.6gi_%.5g_%.5g_res%.7g.png";
+    asprintf(&result, fmt_str, argv[1], frac.formulaStr.c_str(), 
+        frac.cmapStr.c_str(), frac.c_real, frac.c_imag, frac.xmin, frac.ymin, frac.res);
+    }
     cv::imwrite(result, frac.imageR);
   }
   return 0;
